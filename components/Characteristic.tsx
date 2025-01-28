ce7640106d16912a5ee38155180605ee510ae661
Yousef Saadallah <YoussefSaad299@gmail.com>
2025-01-28 23:44:07 +0200

    refactor(ui): update chatbot characteristic UI and button actions

--- components/Characteristic.tsx
+++ components/Characteristic.tsx
@@ -3,6 +3,7 @@
 import { REMOVE_CHARACTERISTIC } from '@/graphql/mutations/mutations';
 import { ChatbotCharacteristic } from '@/types';
 import { useMutation } from '@apollo/client';
+import { Button } from '@heroui/button';
 import { Trash2 } from 'lucide-react';
 import { toast } from 'sonner';
 
@@ -33,14 +34,20 @@ const Characteristic = ({
   };
 
   return (
-    <li className="relative p-10 bg-white border rounded-md">
-      {characteristic.content}
-
-      <Trash2
-        className="size-5 text-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50"
-        onClick={handleRemoveCharacteristic}
-      />
-    </li>
+    <div className="no-underline p-10 hover:opacity-80 active:opacity-disabled z-0 group relative inline-flex items-center justify-center box-border appearance-none subpixel-antialiased overflow-hidden tap-highlight-transparent data-[pressed=true]:scale-[0.97] outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 border-medium rounded-medium [&>svg]:max-w-[theme(spacing.8)] transition-transform-colors-opacity motion-reduce:transition-none bg-transparent border-default data-[hover=true]:opacity-hover group text-sm border-dashed font-normal text-default-600 data-[hover=true]:bg-default-100/50">
+      <p className="font-normal text-medium text-default-500">
+        {characteristic.content}
+      </p>
+      <Button
+        isIconOnly
+        color="danger"
+        variant="light"
+        size="sm"
+        className="absolute top-0 right-0"
+      >
+        <Trash2 onClick={handleRemoveCharacteristic} />
+      </Button>
+    </div>
   );
 };
 export default Characteristic;
